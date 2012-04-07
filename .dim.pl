64ea41e7ed6972828d81567aebf6ba52d8291955
riywo <riywo.jp@gmail.com>
2012-04-07 11:39:55 +0900

    first

---
+++ .dim.pl
@@ -0,0 +1,7 @@
+# This file is managed by /Users/iwanaga.ryosuke/llenv/apps/perl/local/bin/dim.
+{
+  dist => 'TwitterTranslate',
+  distdir => 'TwitterTranslate',
+  module => 'TwitterTranslate',
+  template => 'Default'
+}
---
+++ t/001_basic.t
@@ -0,0 +1,10 @@
+#!perl -w
+use strict;
+use Test::More;
+
+use TwitterTranslate;
+
+# test TwitterTranslate here
+pass;
+
+done_testing;
